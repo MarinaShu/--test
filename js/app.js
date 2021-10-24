e8c33fdce2f357ef280ee33e8c3651ed0001f6f5
MarinaShu <81512618+MarinaShu@users.noreply.github.com>
2021-10-24 14:36:14 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -13,32 +13,13 @@ smoothLinks.forEach(smoothLink => {
     })
 })
 
-// slider
+var input = document.querySelector('.questions__form_phone');
+var placeholder = input.placeholder;
 
-let offset = 0;
-const sliderLine = document.querySelector('.slider__body');
+input.addEventListener('mouseover', () => {
+    input.placeholder = '+7-800-500-25-25'
+})
 
-document.querySelector('#btn-next').addEventListener('click', function () {
-    offset = offset + 425;
-    if (offset > 850) {
-        offset = 0;
-    }
-    sliderLine.style.left = -offset + 'px';
-});
-
-document.querySelector('#btn-prev').addEventListener('click', function () {
-    offset = offset - 425;
-    if (offset < 0) {
-        offset = 850;
-    }
-    sliderLine.style.left = -offset + 'px';
-});
-
-const dots = document.querySelectorAll('.dot');
-
-const activeDot = n => {
-    for (dot of dots) {
-        dot.classList.remove('active');
-    }
-    dots[n].classList.add('active');
-}
+input.addEventListener('mouseout', () => {
+    input.placeholder = placeholder;
+})
